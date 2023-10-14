1450bc589161d967a56a9cb62c12e2558d2ba48c
lucas.kinderknech <lucas.kinderknech@bizitglobal.com>
2023-10-14 01:53:56 -0300

    2do commit levanta back pero no front

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,27 +1,15 @@
-import cohere from "cohere-ai";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+
 import { sendRequest } from "./controller/querie.mjs";
-
-cohere.init("6Du88xd4Cy6uTrWtU2vHzpviyjdZjDyTMon1MA9T");
-
-const response = await cohere.generate({
-  model: "command",
-  prompt: "Hola, buenas!",
-  max_tokens: 300,
-  temperature: 0.9,
-  k:0,
-  stop_sequences: [],
-  return_likelihoods: "NONE",
-
-});
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
