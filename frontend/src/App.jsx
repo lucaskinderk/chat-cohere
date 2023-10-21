68f6df0557a4d632a8b60b0b79812415b694248d
lucas.kinderknech <lucas.kinderknech@bizitglobal.com>
2023-10-21 10:10:18 -0300

    Fix del cohere LK

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [input, setInput] = useState("");
+  const [response, setResponse] = useState("Respuesta...");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const response = await axios.post("http://localhost:3002/api/request", {
+      query: input,
+    });
+    setResponse(response.data);
+    setLoading(false);
+  };
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <section className="section">
+      <div className="container">
+        <h1 className="title">Chat con API de Cohere</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="field">
+            <div className="control">
+              <input
+                className="input"
+                placeholder="¿Qué quieres saber?"
+                type="text"
+                onChange={handleChange}
+                required
+              />
+            </div>
+          </div>
+          <div className="field">
+            <div className="control">
+              <button className="button is-primary" type="submit">
+                Enviar
+              </button>
+            </div>
+          </div>
+        </form>
+        {loading ? <p>Está en carga...</p> : <p>{response}</p>}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </section>
+  );
+};
 
-export default App
+export { App };
